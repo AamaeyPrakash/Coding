17c466184d897e551f3ccc9513f44b894afda8b3
AamaeyPrakash <aamaeyprakash@gmail.com>
2024-04-30 18:15:07 +0530

    Initial commit

---
+++ demoreactapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ChildComponent from './components/ChildComponent';
+
+function App() {
+
+  return (
+    <div>
+      <ChildComponent />
+    </div>
+    );
+}
+
+export default App;
