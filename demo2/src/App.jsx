a780e5ad9e32fd16a7708ab8cc12893d8af3be55
AamaeyPrakash <aamaeyprakash@gmail.com>
2024-05-14 17:21:34 +0530

    navbar

--- demo2/src/App.jsx
+++ demo2/src/App.jsx
@@ -3,7 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from "@/components/ui/button"
-
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,7 +28,19 @@ function App() {
         </select>
       </form> */}
        
-       <Button variant="custom">Button</Button>
+       {/* <Button variant="custom">Button</Button> */}
+       <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Item 1</NavigationMenuTrigger>
+              <NavigationMenuTrigger>Item 2</NavigationMenuTrigger>
+              <NavigationMenuTrigger>Item 3</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink>Link</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
     </>
   )
 }
